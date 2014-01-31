a17a2cd9ef0e0a73d1f38fcf9c33340190830d6a
Russ Tennant <russ@i2rd.com>
2014-01-31 14:16:54 -0600

    Configuration updates for https://github.com/VentureTech/proteusframework/issues/223

---
+++ example-app/src/main/java/com/example/app/config/AspectWeavingTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Interactive Information R & D (I2RD) LLC.
+ * All Rights Reserved.
+ *
+ * This software is confidential and proprietary information of
+ * I2RD LLC ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered
+ * into with I2RD.
+ */
+
+package com.example.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+
+/**
+ * Test if load time weaving is occuring for this package.
+ * @author Russ Tennant (russ@i2rd.com)
+ */
+@Configurable
+public class AspectWeavingTest
+{
+    /** Flag. */
+    private boolean _configured;
+
+    /**
+     * Spring injection.
+     * @param config configuration.
+     */
+    @Autowired
+    public void setAppConfig(MyAppConfig config)
+    {
+        _configured = (config != null);
+    }
+
+    /**
+     * Test if configured.
+     * @return true or false.
+     */
+    public boolean isConfigured()
+    {
+        return _configured;
+    }
+}
