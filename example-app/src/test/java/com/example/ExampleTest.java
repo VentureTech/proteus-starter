ae2662ce13b74071676bc36cdea13516fbe91c18
Jonathan Crosmer <jcrosmer@i2rd.com>
2014-02-27 08:09:47 -0600

    Create ExampleTest.java

---
+++ example-app/src/test/java/com/example/ExampleTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Interactive Information R & D (I2RD) LLC.
+ * All Rights Reserved.
+ *
+ * This software is confidential and proprietary information of
+ * I2RD LLC ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered
+ * into with I2RD.
+ */
+ 
+package org.example;
+
+import org.junit.Test;
+
+/** This example is to prevent Jenkins build from failing due to no unit tests. */
+public class ExampleTest 
+{
+
+    /** Example test method.  */
+    @Test
+    public void test() 
+    {
+    }
+    
+}
