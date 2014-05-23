fd42e064f709d88b737af458499b9a022e52aceb
Russ Tennant <russ@venturetech.net>
2014-05-23 18:09:10 -0500

    PF-606 Save file when saving profile.

--- example-app/src/main/java/com/example/app/model/UserProfileDAO.java
+++ example-app/src/main/java/com/example/app/model/UserProfileDAO.java
@@ -15,22 +15,31 @@ import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.jetbrains.annotations.Contract;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Collection;
 
+import net.proteusframework.cms.CmsSite;
+import net.proteusframework.cms.FileSystemDirectory;
 import net.proteusframework.core.hibernate.dao.DAOHelper;
 import net.proteusframework.core.lang.CloseableIterator;
+import net.proteusframework.data.filesystem.DirectoryEntity;
+import net.proteusframework.data.filesystem.FileEntity;
+import net.proteusframework.data.filesystem.FileSystemDAO;
+import net.proteusframework.data.filesystem.FileSystemEntityCreateMode;
 import net.proteusframework.ui.search.QLBuilder;
 import net.proteusframework.ui.search.QLResolver;
 import net.proteusframework.ui.search.QLResolverOptions;
 
 /**
  * DAO for user profile.
+ *
  * @author Russ Tennant (russ@i2rd.com)
  */
 @Repository
@@ -39,8 +48,13 @@ public class UserProfileDAO extends DAOHelper
     /** Logger. */
     private final static Logger _logger = Logger.getLogger(UserProfileDAO.class);
 
+    /** FileSystem DAO. */
+    @Autowired
+    private FileSystemDAO _fileSystemDAO;
+
     /**
      * Save UserProfile.
+     *
      * @param userProfile the user profile to save.
      */
     public void saveUserProfile(UserProfile userProfile)
@@ -49,13 +63,52 @@ public class UserProfileDAO extends DAOHelper
         boolean success = false;
         try
         {
+            final long id = userProfile.getId();
+            String name = userProfile.getName().getLast() + ", " + userProfile.getName().getFirst();
+            // FIXME : add file extension.
+            String pictureName = name + " #" + id;
             final Session session = getSession();
-            session.saveOrUpdate(userProfile);
+            FileEntity picture = userProfile.getPicture();
+            if (picture != null)
+            {
+                // Ensure our picture file has a unique file name consistent with the profile.
+                if (picture.getId() < 1)
+                {
+                    final CmsSite site = userProfile.getSite();
+                    final DirectoryEntity rootDirectory = FileSystemDirectory.getRootDirectory(site);
+                    DirectoryEntity parentDirectory = _fileSystemDAO.mkdirs(rootDirectory, null, "UserProfilePictures");
+                    picture.setName(pictureName);
+                    picture = _fileSystemDAO.newFile(parentDirectory, picture, FileSystemEntityCreateMode.truncate);
+                    userProfile.setPicture(picture);
+                }
+                else if (!picture.getName().equals(pictureName))
+                {
+                    picture.setName(pictureName);
+                    _fileSystemDAO.update(picture);
+                }
+            }
+
+            if(isTransient(userProfile) || isAttached(userProfile))
+                session.saveOrUpdate(userProfile);
+            else
+                session.merge(userProfile);
+
+            if (picture != null && id == 0)
+            {
+                // New user profile. Update picture name to include the ID
+                pictureName = name + " #" + userProfile.getId();
+                picture.setName(pictureName);
+                _fileSystemDAO.update(picture);
+            }
             success = true;
+        }
+        catch (IOException ioe)
+        {
+            throw new RuntimeException("Unable to access filesystem.", ioe);
         }
         finally
         {
-            if(success)
+            if (success)
                 commitTransaction();
             else
                 recoverableRollbackTransaction();
@@ -64,6 +117,7 @@ public class UserProfileDAO extends DAOHelper
 
     /**
      * Delete the specified user profile.
+     *
      * @param userProfile the user profile to delete.
      */
     public void deleteUserProfile(UserProfile userProfile)
@@ -78,7 +132,7 @@ public class UserProfileDAO extends DAOHelper
         }
         finally
         {
-            if(success)
+            if (success)
                 commitTransaction();
             else
                 recoverableRollbackTransaction();
@@ -87,6 +141,7 @@ public class UserProfileDAO extends DAOHelper
 
     /**
      * Delete the specified user profiles.
+     *
      * @param userProfiles the user profiles to delete.
      */
     public void deleteUserProfiles(Collection<? extends UserProfile> userProfiles)
@@ -96,13 +151,13 @@ public class UserProfileDAO extends DAOHelper
         try
         {
             final Session session = getSession();
-            for(UserProfile userProfile : userProfiles)
+            for (UserProfile userProfile : userProfiles)
                 session.delete(userProfile);
             success = true;
         }
         finally
         {
-            if(success)
+            if (success)
                 commitTransaction();
             else
                 recoverableRollbackTransaction();
@@ -111,6 +166,7 @@ public class UserProfileDAO extends DAOHelper
 
     /**
      * Delete the specified user profiles.
+     *
      * @param qlBuilder a QL builder that will return UserProfiles to delete.
      */
     public void deleteUserProfiles(QLBuilder qlBuilder)
@@ -125,14 +181,14 @@ public class UserProfileDAO extends DAOHelper
             final int atATime = 200;
             options.setFetchSize(atATime);
             queryResolver.setOptions(options);
-            try(CloseableIterator<UserProfile> it = queryResolver.iterate())
+            try (CloseableIterator<UserProfile> it = queryResolver.iterate())
             {
                 int count = 0;
-                while(it.hasNext())
+                while (it.hasNext())
                 {
                     UserProfile userProfile = it.next();
                     session.delete(userProfile);
-                    if(++count > atATime)
+                    if (++count > atATime)
                     {
                         count = 0;
                         session.flush(); // May need to clear action queues as well to free up memory.
@@ -147,7 +203,7 @@ public class UserProfileDAO extends DAOHelper
         }
         finally
         {
-            if(success)
+            if (success)
                 commitTransaction();
             else
                 recoverableRollbackTransaction();
@@ -156,6 +212,7 @@ public class UserProfileDAO extends DAOHelper
 
     /**
      * Evict the specified entity from the session.
+     *
      * @param entity the entity.
      */
     public void evict(Object entity)
@@ -165,6 +222,7 @@ public class UserProfileDAO extends DAOHelper
 
     /**
      * Convert a URL to a URI returning the default value on error.
+     *
      * @param url the URL.
      * @param defaultValue the default value.
      * @return the URI.
@@ -172,7 +230,7 @@ public class UserProfileDAO extends DAOHelper
     @Contract("_,null->null;_,!null->!null")
     public URI toURI(URL url, URI defaultValue)
     {
-        if(url == null) return defaultValue;
+        if (url == null) return defaultValue;
         try
         {
             return url.toURI();
@@ -186,6 +244,7 @@ public class UserProfileDAO extends DAOHelper
 
     /**
      * Convert a URL to a string if possible or return null.
+     *
      * @param link the link.
      * @return the link as a String or null.
      */
