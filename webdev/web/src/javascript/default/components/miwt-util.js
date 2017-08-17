01e9b391ecf55087f7eeb49074a54f39ceae7627
Russ Tennant <russ@proteus.co>
2017-08-17 12:31:03 -0500

    Updates from jarrod

--- webdev/web/src/javascript/default/components/miwt-util.js
+++ webdev/web/src/javascript/default/components/miwt-util.js
@@ -15,41 +15,41 @@
 })
 
  'this' is bound to form element within miwt option functions
+
  */
-(function($){
+(function ($) {
 
     // wrapper property getter/setter
-    function wrap(value, set){
-        return function(newVal){
-            if(newVal !== undefined) value = set(value,newVal)
+    function wrap(value, set) {
+        return function (newVal) {
+            if (newVal !== undefined) value = set(value, newVal);
             return value;
-        }
+        };
     }
 
     // not really function composition
-    function compose(f,g){
+    function compose(f, g) {
         var _this = this;
-        return function(){
+        return function () {
             f.apply(_this, arguments);
             return g.apply(_this, arguments);
-        }
+        };
     }
 
     // composes functions or provides default to oldVal
-    function extend(oldVal, newVal){
-        if(oldVal && oldVal instanceof Function) return compose.call(this, oldVal, newVal);
-        else return newVal || oldVal;
+    function extend(oldVal, newVal) {
+        if (oldVal && oldVal instanceof Function) return compose.call(this, oldVal, newVal);else return newVal || oldVal;
     }
 
-    function init(options){
+    function init(options) {
         // prevents overriding previous submit_options by 'composition'
         var _this = this;
         var submit_options = this.submit_options || {};
         delete this.submit_options; // in case it's already a getter/setter
 
         // prevents future overriding of these submit_options by 'composition'
-        var wrapped = wrap(submit_options, function(oldVal, newVal){
-            for(var i in newVal){
+        var wrapped = wrap(submit_options, function (oldVal, newVal) {
+            for (var i in newVal) {
                 oldVal[i] = extend.call(_this, oldVal[i], newVal[i]);
             }
             return oldVal;
@@ -66,16 +66,15 @@
     }
 
     // only try if jQuery exists
-    $ && $.fn && ($.fn.miwt = function(options){
-        return this.each(function(){
-            init.call(this,options);
+    $ && $.fn && ($.fn.miwt = function (options) {
+        return this.each(function () {
+            init.call(this, options);
         });
     });
 
     // add global, non-jQuery function if no conflict
-    !window.$miwt && (window.$miwt = function(el, options){
-        init.call(el,options);
+    !window.$miwt && (window.$miwt = function (el, options) {
+        init.call(el, options);
         return el;
     });
-
-}(jQuery));
+})(jQuery);
